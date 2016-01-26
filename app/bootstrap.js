961e66772f5df94651e94aa4a3b091c33d753178
uzumaxy <uzumaxy@gmail.com>
2016-01-26 03:51:17 +0200

    App entry point.

---
+++ app/bootstrap.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import express from 'express';
+import { Server } from 'http';
+import socket from 'socket.io';
+
+let app = express();
+let http = Server(app);
+let io = socket(http);
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(4444, () => {
+  console.info('Chat is available on port 4444.')
+});

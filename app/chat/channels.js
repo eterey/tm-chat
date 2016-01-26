4f34f73d23cfd51be7b9014d4ccc69d7757967e4
uzumaxy <uzumaxy@gmail.com>
2016-01-26 04:01:41 +0200

    Added channels module.

---
+++ app/chat/channels.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Channel {
+  constructor(channel) {
+    this.name = channel.name;
+    this.password = channel.password || '';
+    this.users = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  addUser(uuid) {
+    if (this.users.indexOf(uuid) === -1) {
+      this.users.push(uuid);
+    }
+  }
+
+  removeUser(uuid) {
+    let index = this.users.indexOf(uuid);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
+  }
+
+  getUsers() {
+    return this.users;
+  }
+
+  validatePass(pass) {
+    return this.password === pass;
+  }
+}
+
+export default
+class Channels {
+  constructor() {
+    this.channels = {};
+  }
+
+  addChannel(channel) {
+    this.channels[channel.name] = new Channel(channel);
+  }
+
+  getChannel(name) {
+    return this.channels[name];
+  }
+}
